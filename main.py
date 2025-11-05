4c7b36c4f35c0d9e853a68194694259325635477
Lovstroem <lovstroem@gmail.com>
2025-11-05 14:24:00 +0100

    started main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+screen_width = 300
+screen_height = 300
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("TicTacToe")
+
+def draw_grid():
+    bg = (255,255,200)
+    line = (50,50,50)
+    screen.fill(bg)
+    for x in range(1,3):
+        pygame.draw.line(screen, line, (0, x * 100), (screen_width, x * 100), width = 6)
+        pygame.draw.line(screen, line, (x * 100, 0), (x * 100, screen_height), width = 6)
+
+run = True
+while run:
+
+    draw_grid()
+    #add event handlers
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
