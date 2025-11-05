b2d4f97f348d97f360f74ae8be9def66bb2257c3
Lovstroem <lovstroem@gmail.com>
2025-11-05 14:50:36 +0100

    added markers for each box

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ screen_height = 300
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("TicTacToe")
 
+#define variables
+markers = []
+
 def draw_grid():
     bg = (255,255,200)
     line = (50,50,50)
@@ -16,6 +19,12 @@ def draw_grid():
     for x in range(1,3):
         pygame.draw.line(screen, line, (0, x * 100), (screen_width, x * 100), width = 6)
         pygame.draw.line(screen, line, (x * 100, 0), (x * 100, screen_height), width = 6)
+
+for x in range(3):
+    row = [0] * 3
+    markers.append(row)
+
+print(markers)
 
 run = True
 while run:
